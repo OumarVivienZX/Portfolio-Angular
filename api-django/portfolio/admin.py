4c72c941e1d45b794e4f51b37ae97bd416e4c126
OumarVivienZX <KonanOumarvivien@gmail.com>
2026-03-06 11:00:11 +0000

    ajout de données via admin

--- api-django/portfolio/admin.py
+++ api-django/portfolio/admin.py
@@ -11,26 +11,30 @@ class UtilisateurAdmin(admin.ModelAdmin):
     list_filter = ('nom',)
     search_fields = ('nom', 'prenom', 'email')
     ordering = ('id',)
+    list_editable = ('nom', 'prenom', 'email', 'description')
 
 
 @admin.register(Projet)
 class ProjetAdmin(admin.ModelAdmin):
-    list_display = ('id', 'utilisateur', 'resume', 'lien')
+    list_display = ('id', 'utilisateur', 'resume', 'image', 'lien')
     list_filter = ('utilisateur',)
     search_fields = ('resume',)
+    list_editable = ('resume', 'image', 'lien')
 
 
 @admin.register(Experience)
 class ExperienceAdmin(admin.ModelAdmin):
-    list_display = ('id', 'utilisateur', 'role', 'nom_entreprise', 'date_debut', 'date_fin')
+    list_display = ('id', 'utilisateur', 'role', 'nom_entreprise', 'date_debut', 'date_fin', 'type_de_contrat')
     list_filter = ('utilisateur',)
     search_fields = ('role', 'nom_entreprise')
+    list_editable = ('role', 'nom_entreprise', 'date_debut', 'date_fin', 'type_de_contrat')
 
 
 @admin.register(Service)
 class ServiceAdmin(admin.ModelAdmin):
-    list_display = ('id', 'utilisateur', 'type_de_service', 'details')
+    list_display = ('id', 'utilisateur', 'type_de_service', 'details', 'outils')
     list_filter = ('utilisateur',)
+    list_editable = ('type_de_service', 'details', 'outils')
 
 
 @admin.register(PriseDeContact)
@@ -44,9 +48,11 @@ class PriseDeContactAdmin(admin.ModelAdmin):
 class ReseauSocialAdmin(admin.ModelAdmin):
     list_display = ('id', 'utilisateur', 'nom_platform', 'lien')
     list_filter = ('utilisateur',)
+    list_editable = ('nom_platform', 'lien')
 
 
 @admin.register(Localisation)
 class LocalisationAdmin(admin.ModelAdmin):
     list_display = ('id', 'utilisateur', 'ville', 'pays', 'quartier')
     list_filter = ('utilisateur',)
+    list_editable = ('ville', 'pays', 'quartier')
