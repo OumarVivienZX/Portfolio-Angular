66b99590e4dc1c6b76664f295a01f4d0307ef9a9
OumarVivienZX <KonanOumarvivien@gmail.com>
2026-03-06 08:10:00 +0000

    déploiement en production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,10 +4,5 @@
  */
 export const environment = {
   production: true,
-  /**
-   * En déploiement séparé (Netlify pour Angular + backend ailleurs),
-   * renseigner ici l'URL publique du backend Django.
-   * Exemple: https://portfolio-api.onrender.com/api/
-   */
-  apiUrl: 'https://CHANGE_ME_BACKEND_DOMAIN/api/'
+  apiUrl: 'https://portfolio-angular-ezot.onrender.com/api/' // url en production
 };
